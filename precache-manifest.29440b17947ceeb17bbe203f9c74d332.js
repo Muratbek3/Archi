36102d4c09dab2f26ef71527a5f57f3bad798900
Muratbek3 <45931103+Muratbek3@users.noreply.github.com>
2021-03-10 10:50:16 +0500

    Add files via upload

---
+++ precache-manifest.29440b17947ceeb17bbe203f9c74d332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea89da3830977130f6e8ce81cd307757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d78134133e618e4619c1",
+    "url": "/static/css/main.812602da.chunk.css"
+  },
+  {
+    "revision": "81a2e6bff607c5274a55",
+    "url": "/static/js/2.8b5dbf2e.chunk.js"
+  },
+  {
+    "revision": "d78134133e618e4619c1",
+    "url": "/static/js/main.fc5f5762.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
